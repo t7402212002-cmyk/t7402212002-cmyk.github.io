54f0813d43ab39d42eaa3e075301e62e15459876
t7402212002-cmyk <t7402212002@gmail.com>
2026-06-01 17:24:44 +0800

    更新

---
+++ tools/demo/src/settings.05611.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/demo_01.fire",orientation:"",jsList:[],bundleVers:{internal:"efe26",main:"d9dca"}};
